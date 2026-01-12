4a5bedd142fffc6f6072b33b544a71930b3fdd5b
teacherbot <norrisa@faytechcc.edu>
2026-01-12 18:57:25 +0000

    started m1t1

---
+++ m1t1_lastname.cpp
@@ -0,0 +1,13 @@
+// CSC 134
+// M1T1 (Module 1, tutorial 1)
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Say hello
+    cout << "Hello, World!" << endl;
+    cout << endl;  // blank line
+    return 0; // exit with no errors
+}

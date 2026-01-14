ed4d399d87dedfb13b4d1574e28528b674597218
teacherbot <norrisa@faytechcc.edu>
2026-01-14 16:26:39 +0000

    finished m1t1

--- m1t1_0001.cpp
+++ m1t1_0001.cpp
@@ -11,11 +11,12 @@ int main() {
     cout << "Put anything you want here!" << endl;
     cout << endl; // another new line
 
-    /*
-    next we will:
-    - make a variable (a noun) to store the user's name
-    - ask them for the name
-    - greet them by name
-    */
+    string name; // new "box" to hold a name in
+    cout << "What's your name? ";
+    cin >> name; // read in their answer
+    // finally, say hi to them
+    cout << "Well hi, " << name << "!" << endl;
+
+
     return 0; // no errors
 }

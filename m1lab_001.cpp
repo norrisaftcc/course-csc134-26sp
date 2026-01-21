316df7831be4724a5d86e6e0c37053ebcd5f8161
teacherbot <norrisa@faytechcc.edu>
2026-01-21 16:09:02 +0000

    m1lab

--- m1lab_001.cpp
+++ m1lab_001.cpp
@@ -1,5 +1,5 @@
 // CSC 134
-// M1LAB1
+// M1LAB1 - We're selling something
 // name
 // date
 
@@ -7,5 +7,21 @@
 using namespace std;
 
 int main() {
+    // Declare my variables
+    string item_name = "apples";    // use whatever you want instead
+    int item_count = 100;           // how many we have
+    double item_cost = 0.25;        // price per item
+    double total_cost;              // don't know yet
+
+    // Do the work - Welcome them to the store.
+    cout << "Welcome to the " << item_name << " store." << endl;
+    cout << "We have " << item_count << " " << item_name << "." << endl;
+    cout << "They cost $" << item_cost << " each." << endl;
+
+    // Calculate the total price
+    total_cost = item_count * item_cost; 
+    // Print the total
+    cout << "To buy all of them will cost $" << total_cost << endl;
+
     return 0;
 }

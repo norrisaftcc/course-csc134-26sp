cfbfc388302bea08228c0d46e9f87bf7d85152a8
teacherbot <norrisa@faytechcc.edu>
2026-01-28 15:52:26 +0000

    added variables

--- m2lab1_0001.cpp
+++ m2lab1_0001.cpp
@@ -13,6 +13,22 @@ Print everything out
 #include <iomanip>
 
 int main() {
+    // Part 1 -- Declare Variables
+    // constant variables (don't change unless the market changes)
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+
+    // Crate variables 
+    double length, width, height, volume; // all in feet
+    // Money variables
+    double cost, customer_price, profit;  // all in $
+
+    // Part 2 -- Get the Input
+
+    // Part 3 -- Do the Calculations
+
+    // Part 4 -- Print the Output
+
 
     return 0; 
 }

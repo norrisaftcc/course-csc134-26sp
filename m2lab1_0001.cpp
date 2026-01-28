594bcdabbf87e77253dbdaad81aa216417ee1c55
teacherbot <norrisa@faytechcc.edu>
2026-01-28 15:32:53 +0000

    started m2lab1

---
+++ m2lab1_0001.cpp
@@ -0,0 +1,18 @@
+/*
+CSC 134
+M2Lab1 - The Crate Program
+norrisa
+1/28/26
+
+Find volume of crate
+Find cost, customer_price, and profit
+Print everything out
+*/
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+
+    return 0; 
+}

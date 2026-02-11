d9a702d1f1116ace0d046e42c4f39a6448f9a9e1
teacherbot <norrisa@faytechcc.edu>
2026-02-11 15:28:54 +0000

    started m3t1

---
+++ m3/m3t1_lastname.cpp
@@ -0,0 +1,20 @@
+// M3T1, CSC 134, norrisa, 2/11/26
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // As a user, I want to measure rectangles to find out how big they are.
+
+    // Set up variables
+    double length, width, area;
+
+    // Ask user for info
+    cout << "What is the rectangle's length? ";
+    cin >> length;
+
+    // Do calculations
+
+    // Print the answer
+
+}

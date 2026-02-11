462aa62e8be6aae6440686537f455da6e8c1ec97
teacherbot <norrisa@faytechcc.edu>
2026-02-11 15:36:08 +0000

    works for 2 rectangles

--- m3/m3t1_lastname.cpp
+++ m3/m3t1_lastname.cpp
@@ -7,14 +7,26 @@ int main() {
     // As a user, I want to measure rectangles to find out how big they are.
 
     // Set up variables
-    double length, width, area;
+    double length1, width1, area1;
+    double length2, width2, area2;
 
     // Ask user for info
-    cout << "What is the rectangle's length? ";
-    cin >> length;
+    cout << "What is the first rectangle's length? ";
+    cin >> length1;
+    cout << "What is the width? ";
+    cin >> width1;
+
+    cout << "What is the second rectangle's length? ";
+    cin >> length2;
+    cout << "What is the width? ";
+    cin >> width2;
 
     // Do calculations
+    area1 = length1 * width1;
+    area2 = length2 * width2;
 
     // Print the answer
+    cout << "Rectangle #1: " << length1 << " by " << width1 << " is area: " << area1 << endl;
+    cout << "Rectangle #2: " << length2 << " by " << width2 << " is area: " << area2 << endl;
 
 }

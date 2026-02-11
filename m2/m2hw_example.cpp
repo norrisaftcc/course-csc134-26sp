5453b733be31a1a193845cdeb80f50521932284c
teacherbot <norrisa@faytechcc.edu>
2026-02-11 18:29:57 +0000

    started m3t1

---
+++ m2/m2hw_example.cpp
@@ -0,0 +1,23 @@
+/*
+M2HW Example
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+int main() {
+
+    cout << "Question 1" << endl;
+
+    // Write question one code here
+
+    // Question 2
+    cout << "Question 2" << endl;
+
+    // Question 3
+    cout << "Question 3" << endl;
+
+}

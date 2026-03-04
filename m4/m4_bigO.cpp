1223ef42175bfce95623832b91192f990214d63b
teacherbot <norrisa@faytechcc.edu>
2026-03-04 18:36:09 +0000

    m4t1 done

---
+++ m4/m4_bigO.cpp
@@ -0,0 +1,21 @@
+// big-O examples
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int size;
+    cout << "How big is the loop? ";
+    cin >> size;
+
+    cout << "Starting..." << endl;
+    for (int i=0; i < size; i++) {
+        for (int j=0; j < size; j++) {
+            int x = 0; // just do something
+        }
+         
+    }
+    cout << "Done." << endl;
+
+}

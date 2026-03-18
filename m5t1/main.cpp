ac252bae0d5595140e6a9aae2f4870492b333556
teacherbot <norrisa@faytechcc.edu>
2026-03-18 11:04:42 -0400

    Add files via upload

--- m5t1/main.cpp
+++ m5t1/main.cpp
@@ -15,12 +15,26 @@ int  double_a_num(int number);  // int arg, returns int
 int main()
 {
     say_hello();
+    cout << "The answer is: " << get_answer() << endl;
+    cout << "Type in an integer: ";
+    int num;
+    cin >> num;
+    int answer = double_a_num(num);
+    cout << "Double that number is: " << answer << endl;
     return 0;
 }
-
 
 // Define our functions (full code goes here)
 
 void say_hello() {
     cout << "Hello world!" << endl;
+}
+
+int get_answer() {
+    return 42;
+}
+
+int double_a_num(int number) {
+    int answer = number * 2;
+    return answer;
 }

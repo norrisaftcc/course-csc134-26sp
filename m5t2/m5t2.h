fc2e40cd896b4c2bb070be60c6d398f166ec6c84
teacherbot <norrisa@faytechcc.edu>
2026-03-23 14:28:36 +0000

    example m5t2 header

---
+++ m5t2/m5t2.h
@@ -0,0 +1,17 @@
+#ifndef M5T2_H_INCLUDED
+#define M5T2_H_INCLUDED
+
+int square(int num) {
+    // input: a number
+    // output: the number, squared
+
+    return 0; // TODO
+}
+
+void print_table_line(int first, int second) {
+    // input: two numbers
+    // effect: prints a line with both numbers
+
+}
+
+#endif // M5T2_H_INCLUDED

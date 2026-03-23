3037738befbb0de2b7baa5cf06c38df9b43b6f2f
teacherbot <norrisa@faytechcc.edu>
2026-03-23 14:34:37 +0000

    updated functions

--- m5t2/m5t2.h
+++ m5t2/m5t2.h
@@ -4,14 +4,14 @@
 int square(int num) {
     // input: a number
     // output: the number, squared
-
-    return 0; // TODO
+    int answer = num * num;
+    return answer; 
 }
 
 void print_table_line(int first, int second) {
     // input: two numbers
     // effect: prints a line with both numbers
-
+    cout << first << "\t" << second << endl;
 }
 
 #endif // M5T2_H_INCLUDED

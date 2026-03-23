4037363fb28f5e863d6a1c91cccc7e97b942a3d1
teacherbot <norrisa@faytechcc.edu>
2026-03-23 14:38:23 +0000

    latest

--- m5t2/m5t2.h
+++ m5t2/m5t2.h
@@ -1,11 +1,14 @@
 #ifndef M5T2_H_INCLUDED
 #define M5T2_H_INCLUDED
 
+#include <iostream>
+using namespace std;
+
 int square(int num) {
     // input: a number
     // output: the number, squared
     int answer = num * num;
-    return answer; 
+    return answer;
 }
 
 void print_table_line(int first, int second) {

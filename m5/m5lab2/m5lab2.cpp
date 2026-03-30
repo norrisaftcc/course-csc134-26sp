b65027d6bc183de4ebbf0b6a9c96250b6ca9d055
teacherbot <norrisa@faytechcc.edu>
2026-03-30 14:21:56 +0000

    m5lab2 starter

---
+++ m5/m5lab2/m5lab2.cpp
@@ -0,0 +1,53 @@
+// M5LAB2
+// Finishing someone's work -- Area of a rectangle
+// norrisa
+// 3/30/26
+
+#include <iostream>
+using namespace std;
+
+// Declare (Write the prototypes for)
+// the getLength,
+// getWidth, getArea, and displayData
+// functions here.
+double getLength();
+double getWidth();
+double getArea(double length, double width);
+void   displayData(double length, double width, double area);
+
+int main()
+{
+	// This program calculates the area of a rectangle.
+	// TODO: fix any syntax errors
+	
+   double length,    // The rectangle's length
+          width,     // The rectangle's width
+          area;      // The rectangle's area
+          
+   // Get the rectangle's length.
+   length = getLength();
+   
+   // Get the rectangle's width.
+   width = getWidth();
+   
+   // Get the rectangle's area.
+   area = getArea(length, width);
+   
+   // Display the rectangle's data.
+   displayData(length, width, area);
+          
+   return 0;
+}
+
+//***************************************************
+// TODO: write the getLength, getWidth, getArea,    *
+// and displayData functions below.                 *
+//***************************************************
+
+double getLength() {
+    // getLength - Asks the user to enter a rectangle's length, and return that value as a double.
+    double length;
+    cout << "Enter the rectangle's length: ";
+    cin >> length;
+    return length;
+}
